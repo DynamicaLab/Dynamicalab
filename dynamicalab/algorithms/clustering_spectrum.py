84107771e498a2ac9900da7bff8008df5b8b839b
Antoine Allard <antoine.allard.1@gmail.com>
2018-08-20 14:54:35 -0400

    Added details to the documentation and a more detailled example.

--- dynamicalab/algorithms/clustering_spectrum.py
+++ dynamicalab/algorithms/clustering_spectrum.py
@@ -17,10 +17,10 @@ def clustering_spectrum(g):
         The graph must be **simple** (no multiedges), **undirected** and **without self-loops**.
       
 
-      **Retuns**
+      **Returns**
 
       clust_spect : dict
-          Dictionary mapping each degree class to the corresponding average local clustering coefficient.
+          Dictionary mapping each degree class (integers) to the corresponding average local clustering coefficient (float).
 
 
       **Example**
@@ -28,11 +28,26 @@ def clustering_spectrum(g):
 
       .. code:: python
 
+        import numpy as np
         import networkx as nx
+        import matplotlib.pyplot as plt
         import dynamicalab.algorithms as algo
 
-        G = nx.florentine_families_graph()
-        onion, kcore = algo.clustering_spectrum(G)
+        # Gets a network and extracts its clustering spectrum.
+        G = nx.karate_club_graph()
+        clust_spect = algo.clustering_spectrum(G)
+
+        # Plots the clustering spectrum.
+        fig, ax = plt.subplots()
+        plt.bar(clust_spect.keys(), clust_spect.values(), width=0.50, color='g')
+
+        plt.title("Clustering spectrum")
+        ax.set_ylabel("Average local clustering")
+        ax.set_xlabel("Degree")
+        ax.set_ylim(bottom=0, top=1)
+        ax.set_xticks(np.arange(max(clust_spect.keys()) + 1))
+
+        plt.show()
       
 
     """
